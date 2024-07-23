370a7baf301cb37d20ecdec1919de60cbfce7682
sirijear <sirijear@gmail.com>
2024-07-23 16:13:09 +0700

    new button code

--- GitBlink.ino
+++ GitBlink.ino
@@ -1,14 +1,18 @@
 const int ledPin = 13;
-const int delayTime = 500; 
+const int delayTime = 500;
+const int buttonPin = 7;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH);
-  delay(delayTime);
-  digitalWrite(ledPin, LOW);
-  delay(delayTime);
+  // only run when button is not pressed
+  if (digitalRead(buttonPin) == HIGH) {
+    digitalWrite(ledPin, HIGH);
+    delay(delayTime);
+    digitalWrite(ledPin, LOW);
+    delay(delayTime);
+  }
 }
-
